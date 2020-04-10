90a6053773ae55f28210a34b96b2a2b73dc5a8fa
Jacob Lewis <54906247+Stikcyee@users.noreply.github.com>
2020-04-09 22:18:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # authors: stickyee and joshuathooyavan
 
 # FIRST API
-username = 'stikcye'
-password = '9933C5D2-F724-428F-B4A6-6A8978A3F251'
+username = ''
+password = ''
 
 # DISCORD BOT
-bot_key = 'Njk2OTYxNTUzMDE4Mzg4NDkw.Xo0Vqg.BZvCb_agYbVJRXA1XHQ8sQs7FJU'
+bot_key = ''
 bot_prefix = '-'
